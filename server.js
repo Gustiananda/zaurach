e6c5b4ce27e84cc2231adf7ed508849988df400d
unknown <barongusti@gmail.com>
2023-07-30 21:45:20 +0700

    menambahkan uploads

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 import orderRouter from "./routes/orderRoute.js";
-import fs from 'fs'
+import fs from "fs";
+import path from "path";
 //configure env
 dotenv.config();
 
@@ -22,8 +23,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client-app/build")));
 
-const folders = ['uploads/pdf'];
+const folders = ["uploads/pdf"];
 for (const folder of folders) {
   if (!fs.existsSync(folder)) {
     fs.mkdirSync(folder, { recursive: true });
@@ -35,12 +37,13 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/order", orderRouter);
+
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client-app/build/index.html"));
 });
 // Set Public Folder
-app.use('/uploads/pdf', express.static('uploads/pdf'));
+app.use("/uploads/pdf", express.static("uploads/pdf"));
 
 //PORT
 const PORT = process.env.PORT || 8080;

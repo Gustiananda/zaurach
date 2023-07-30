27992abd9e96346182ee0d3493a7cdf6043214d9
Gustiananda <93473791+Gustiananda@users.noreply.github.com>
2023-07-30 21:52:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,17 @@ import productRoutes from "./routes/productRoutes.js";
 import orderRouter from "./routes/orderRoute.js";
 import fs from "fs";
 import path from "path";
+import {fileURLToPath} from 'url';
+
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
